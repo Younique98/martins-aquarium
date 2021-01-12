2dda0644949817f49d94fa72f33e8f815bc017ac
Erica Thompson <younique98@gmail.com>
2021-01-12 13:17:59 -0800

    deleted fish list component from fish.js and added new folder

--- scripts/Fish.js
+++ scripts/Fish.js
@@ -10,15 +10,3 @@ export const Fish = (fish) => {
     </section>
     `
 }
-export const FishList = () => {
-    // Get a reference to the `<article class="content">` element
-    const contentElement = document.querySelector("insert selector here")
-    const fishes = useFish()
-
-    // Add to the existing HTML in the content element
-    contentElement.innerHTML += `
-        <article class="fishList">
-            All the fish go here!
-        </article>
-    `
-} 
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 console.log("Welcome to the main module")
 
+import { FishList } from './Fish.js'
 import { useFish } from './FishDataProvider.js'
 
 const allTheFish = useFish()
@@ -7,3 +8,5 @@ const allTheFish = useFish()
 for (const fish of allTheFish) {
     console.log(fish)
 }
+// Import the FishList here
+FishList()
